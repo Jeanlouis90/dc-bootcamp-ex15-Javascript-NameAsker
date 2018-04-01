9ebfb53b45f0dfb4fd954577dbfbdd8ea67557f1
Jeanlouis90 <jeanlouisjabouin@gmail.com>
2018-03-31 21:43:42 -0400

    First commit. NameAsker is a simple JS code that asks the user what his/her name is, and the user responds.

---
+++ script.js
@@ -0,0 +1,6 @@
+var firstName = prompt("What is your first name?");
+var lastName = prompt("What is your last name, " + firstName + "?");
+var fullName = firstName + " " + lastName;
+console.log("Your Name is " + fullName + ".");
+var age = prompt("How old are you, " + firstName + "?");
+console.log("You are " + age + ".");
